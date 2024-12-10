30a3eb25c0c645918f2311f5e18c2d48cf2fcc0e
nivesayee <nivesayee@gmail.com>
2024-12-10 23:27:36 +0000

    Changes to day 4 solution

--- day4.sql
+++ day4.sql
@@ -1,6 +1,6 @@
 select toy_id,toy_name,array(select unnest(new_tags) except select unnest(previous_tags)) as added_tags,
 array(select unnest(new_tags) intersect select unnest(previous_tags)) as unchanged_tags,
 array(select unnest(previous_tags) except select unnest(new_tags)) as removed_tags,
-coalesce(array_length(array(select unnest(new_tags) except select unnest(previous_tags)),1),0) as mlen from toy_production
-order by mlen desc
+coalesce(array_length(array(select unnest(new_tags) except select unnest(previous_tags)),1),0) as added_tags_len from toy_production
+order by added_tags_len desc
 limit 1;
